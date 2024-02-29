608db7bf211e67ec40b09a08c29e15e06fa6ff87
Your Name <you@example.com>
2024-02-29 22:24:37 +0800

    test

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -16,6 +16,7 @@ export default (data) => {
     srcDir: "./src",
     assetsDir: './assets',
     outDir: './docs',
+    base: '/notes',
 
     sitemap: {
       hostname: 'https://notes.chdl.fun',
