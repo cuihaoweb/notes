3416526634f7b84abe4df450615d7ffd8581f23d
cuihaoweb <58589917+cuihaoweb@users.noreply.github.com>
2024-02-29 13:59:31 +0000

    Deploying to rb from @ cuihaoweb/notes@2fd989fd790f9bf9cccd9cd57edd4336b341efed 🚀

---
+++ .vitepress/config.mjs
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vitepress';
+import ElementPlus from 'unplugin-element-plus/vite'
+
+export default (data) => {
+  const config = defineConfig({
+    vite: {
+      ssr: {
+        noExternal: [/^element-plus/],
+      },
+      plugins: [
+        ElementPlus()
+      ]
+    },
+
+    // 文件相关
+    srcDir: "./src",
+    assetsDir: './assets',
+    outDir: './dist',
+
+    sitemap: {
+      hostname: 'https://notes.chdl.fun',
+    },
+
+    // 页面布局相关
+    lastUpdated: true,
+    title: 'ch-notes',
+    head: [
+      [
+        'link', {
+          rel: 'icon',
+          type: "image/svg+xml",
+          href: '/logo.png'
+        }
+      ]
+    ],
+    themeConfig: {
+      logo: '/logo.png',
+      search: {
+        provider: 'local'
+      },
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Examples', link: '/markdown-examples' }
+      ],
+      // sidebar: [
+      //   // {
+      //   //   text: 'Examples',
+      //   //   items: [
+      //   //     { text: 'Markdown Examples', link: '/markdown-examples' },
+      //   //     { text: 'Runtime API Examples', link: '/api-examples' }
+      //   //   ]
+      //   // }
+      // ],
+      socialLinks: [
+        { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      ]
+    }
+  });
+  return config;
+};
---
+++ .vitepress/theme/index.js
@@ -0,0 +1,3 @@
+import theme from '@notes/theme';
+
+export default theme;
