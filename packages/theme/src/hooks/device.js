a04ddf1987a67f84833e56ddd870d6d360475219
Your Name <you@example.com>
2024-03-01 22:45:03 +0800

    test

--- packages/theme/src/hooks/device.js
+++ packages/theme/src/hooks/device.js
@@ -6,15 +6,21 @@ const deviceInfo = reactive({
     isH5: false
 });
 
+function handleResize() {
+    deviceInfo.isPc = window.innerWidth > 768;
+    deviceInfo.isH5 = !deviceInfo.isPc;
+}
+
+if (inBrowser) {
+    handleResize();
+}
+
 /**
  * 获取设备信息
  * @return {typeof deviceInfo}
  */
 export function useDevice() {
-    function handleResize() {
-        deviceInfo.isPc = window.innerWidth > 768;
-        deviceInfo.isH5 = !deviceInfo.isPc;
-    }
+    
 
     onMounted(() => {
         handleResize();
